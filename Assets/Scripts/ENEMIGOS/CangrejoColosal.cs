a882eb4925d28846acc80ab73d45ac81e291594d
CresPlayer <jbuenavida02@gmail.com>
2025-05-01 17:18:52 +0200

    casads

---
+++ Assets/Scripts/ENEMIGOS/CangrejoColosal.cs
@@ -0,0 +1,168 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CangrejoColosal : MonoBehaviour
+{
+    public float speed;
+    public Transform controladorSuelo;
+    public float distancia;
+    private bool moviendoDerecha;
+    private Rigidbody2D rb;
+
+    protected bool AtaquePinzas;
+    protected bool AtaquePinchos;
+
+    public float fuerzaRebote;
+    public int vidas;  // Vidas del enemigo
+    public GameObject experienciaPrefab;
+    public int experienciaSoltar = 20;
+    protected bool playervivo;
+    protected bool muerto;
+    protected bool recibiendoDanio;
+    protected bool isFrozen = false;
+    protected float originalSpeed;
+    protected SpriteRenderer spriteRenderer;
+    protected Animator animator;
+
+    protected void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playervivo = true;
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalSpeed = speed;
+    }
+
+    protected void FixedUpdate()
+    {
+        if (!muerto && !isFrozen)
+        {
+            RaycastHit2D informacionSuelo = Physics2D.Raycast(controladorSuelo.position, Vector2.down, distancia);
+
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+
+            if (informacionSuelo == false)
+            {
+                Girar();
+            }
+        }
+        else if (isFrozen)
+        {
+            rb.velocity = Vector2.zero; // Asegura que no siga con velocidad anterior
+        }
+    }
+
+    protected void Girar()
+    {
+        moviendoDerecha = !moviendoDerecha;
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
+        speed *= -1;
+    }
+
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Espada"))
+        {
+            Debug.Log("golpe con espada");
+            Vector2 direcciondanio = new Vector2(collision.gameObject.transform.position.x, 0);
+
+            RecibeDanio(direcciondanio, 1);
+        }
+        if (collision.CompareTag("Pegaso"))
+        {
+            Vector2 direcciondanio = new Vector2(collision.gameObject.transform.position.x, 0);
+
+            RecibeDanio(direcciondanio, 1);
+        }
+    }
+    public void RecibeDanio(Vector2 direccion, int cantDanio)
+    {
+        if (!recibiendoDanio)
+        {
+            recibiendoDanio = true;
+            animator.SetBool("recibiendoDanio", true);
+
+            // Reducir las vidas del enemigo
+            vidas -= cantDanio;
+            //animator.SetBool("AtaqueCola", false);
+
+            // Si las vidas son 0 o menos, destruir al enemigo
+            if (vidas <= 0)
+            {
+                Muerte();
+            }
+            else
+            {
+                // Si no ha muerto, aplicar el rebote
+                //Vector2 rebote = new Vector2(transform.position.x - direccion.x, 1).normalized;
+                //rb.AddForce(rebote * fuerzaRebote, ForceMode2D.Impulse);
+            }
+
+
+            StartCoroutine(DesactivarDanio());
+        }
+    }
+    IEnumerator DesactivarDanio()
+    {
+        yield return new WaitForSeconds(0.2f);
+        recibiendoDanio = false;
+        animator.SetBool("recibiendoDanio", false);
+
+    }
+
+    protected void Muerte()
+    {
+        rb.velocity = Vector2.zero;
+        muerto = true;
+        animator.SetBool("EstaMuerto", true);
+        //if (experienciaPrefab != null)
+        //{
+        //Vector3 posicion = transform.position;
+        //GameObject Experiencia = Instantiate(experienciaPrefab, posicion, experienciaPrefab.transform.rotation);
+        //Experiencia.GetComponent<Experiencia>().cantidadExperiencia = experienciaSoltar;
+        //}
+        // Puedes agregar animaciones de muerte aquí si lo deseas
+        // Por ejemplo: animator.SetTrigger("Muerte");
+
+        StartCoroutine(EsperarMuerte());
+    }
+    IEnumerator EsperarMuerte()
+    {
+        // Espera el tiempo de duración de la animación de muerte anticipada
+        yield return new WaitForSeconds(0.683f); // Ajusta este tiempo según la duración de la animación
+        Destroy(gameObject); // Destruir el enemigo después de que la animación haya terminado
+    }
+    public void Freeze(float duration)
+    {
+        if (!isFrozen)
+        {
+            isFrozen = true;
+            speed = 0;
+            rb.velocity = Vector2.zero;
+            spriteRenderer.color = Color.blue;
+            if (animator != null)
+            {
+                animator.enabled = false;
+            }
+            StartCoroutine(UnfreezeAfterTime(duration));
+        }
+    }
+
+    IEnumerator UnfreezeAfterTime(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        isFrozen = false;
+        speed = originalSpeed;
+        spriteRenderer.color = Color.white;
+        if (animator != null)
+        {
+            animator.enabled = true;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(controladorSuelo.transform.position, controladorSuelo.transform.position + Vector3.down * distancia);
+    }
+}
