a3818b12042db51bcea2abe5f0a4dbe6ecbeb42e
CresPlayer <jbuenavida02@gmail.com>
2025-05-04 23:29:08 +0200

    asid

---
+++ Assets/Scripts/CambiarEscena1.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;  // Necesario para cargar escenas
+
+public class CambiarEscena1 : MonoBehaviour
+{
+    // Este método se llama cuando otro collider entra en el trigger
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Aquí puedes comprobar si el objeto que entra al trigger es el correcto, por ejemplo si es el jugador
+        if (other.CompareTag("Player"))
+        {
+            // Cambiar a la escena con el nombre especificado
+            SceneManager.LoadScene("Creditos");
+        }
+    }
+}
