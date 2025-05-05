7de63f8eb783d57903622e872ea7063b357b1242
Joel  Buenavida Crespo <joel.buenavida@enti.cat>
2025-05-05 10:14:25 +0200

    menu pausa bug

--- Assets/Scripts/MenuPausa.cs
+++ Assets/Scripts/MenuPausa.cs
@@ -12,7 +12,7 @@ public class MenuPausa : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        juegoPausado = false;
+        Reanudar();
     }
 
     // Update is called once per frame
